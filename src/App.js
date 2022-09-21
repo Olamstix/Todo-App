e3b944b378296688a272f722c8406bcf64c32d27
riancarlosdev <riancontatoprofissional@gmail.com>
2022-09-21 03:36:17 -0300

    updated riancarlosdev

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import TodoList from "./components/TodoList";
+import { TodoList } from "./components/TodoList";
 
 export const App = () => (
   <div id="page-container">
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,58 +1,178 @@
-import React, { useState } from "react";
-import TodoForm from "./TodoForm";
-import Todo from "./Todo";
+import React, { useCallback, useState } from "react";
+import { InputComponent } from "./Input";
+import {
+  AiOutlineDelete,
+  AiOutlineEdit,
+  AiFillDelete,
+  AiFillEdit,
+  AiOutlineSave,
+  AiFillSave,
+  AiOutlineCloseSquare,
+  AiFillCloseSquare,
+} from "react-icons/ai";
 
-function TodoList() {
+export const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const [valueAdd, setValueAdd] = useState("");
+  const [edit, setEdit] = useState("");
+  const [editValue, setEditValue] = useState("");
 
-  const addTodo = (todo) => {
-    if (!todo.text || /^\s*$/.test(todo.text)) {
+  const addTodo = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (valueAdd === "") {
+        return;
+      }
+
+      const newValue = valueAdd.replace(/( )+/g, " ");
+      if (newValue === " ") {
+        return;
+      }
+
+      if (todos.find((todo) => todo.value === newValue)) {
+        return;
+      }
+
+      setValueAdd("");
+      setTodos([
+        { value: newValue, key: todos.length + 1 + "", isComplete: false },
+        ...todos,
+      ]);
+    },
+    [todos, valueAdd]
+  );
+
+  const updateTodo = useCallback(() => {
+    if (editValue === "") {
       return;
     }
-    const newTodos = [todo, ...todos];
 
-    setTodos(newTodos);
-  };
-
-  const updateTodo = (todoId, newValue) => {
-    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+    const newValue = editValue.replace(/( )+/g, " ");
+    if (newValue === " ") {
       return;
     }
-    setTodos((prev) =>
-      prev.map((item) => (item.id === todoId ? newValue : item))
-    );
-  };
 
-  const removeTodo = (id) => {
-    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    if (todos.find((todo) => todo.value === newValue)) {
+      return;
+    }
 
-    setTodos(removeArr);
-  };
-
-  const completeTodo = (id) => {
-    let updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
+    const nextState = todos.map((todo) => {
+      if (todo.key === edit) {
+        todo.value = newValue;
       }
       return todo;
     });
-    setTodos(updatedTodos);
-  };
+    setEdit("");
+    setEditValue("");
+    setTodos(nextState);
+  }, [edit, editValue, todos]);
+
+  const checkTodo = useCallback(
+    (key) => {
+      const nextState = todos.map((todo) => {
+        if (todo.key === key) {
+          todo.isComplete = !todo.isComplete;
+        }
+        return todo;
+      });
+      setTodos(nextState);
+    },
+    [todos]
+  );
 
   return (
     <div id="container">
       <div id="container__header">
         <h1 id="container__header__title"> What 's the Plan for Today</h1>
       </div>
-      <TodoForm onSubmit={addTodo} />{" "}
-      <Todo
-        todos={todos}
-        completeTodo={completeTodo}
-        removeTodo={removeTodo}
-        updateTodo={updateTodo}
-      />{" "}
+      <form onSubmit={addTodo}>
+        <div id="container__add">
+          <InputComponent
+            props={{
+              name: "add-todo",
+              handleChange: setValueAdd,
+              value: valueAdd,
+              placeholder: "Add a todo",
+            }}
+          />
+          <button type="submit" className="todo__button add">
+            add new
+          </button>
+        </div>
+      </form>
+      <div id="container__content">
+        <ul>
+          {todos?.map((todo) => (
+            <li
+              className={todo.isComplete ? "todo__item complete" : "todo__item"}
+              key={todo.key}
+            >
+              <div
+                onClick={
+                  edit !== todo.key ? () => checkTodo(todo.key) : undefined
+                }
+                className={`todo__item__value`}
+                key={todo.id}
+              >
+                {edit !== todo.key ? (
+                  <span>{todo.value}</span>
+                ) : (
+                  <input
+                    autoFocus={true}
+                    className="item__input__edit"
+                    value={editValue}
+                    onKeyDown={(e) => e.key === "Enter" && updateTodo()}
+                    onChange={(e) => {
+                      setEditValue(e.target.value);
+                    }}
+                    type="text"
+                  />
+                )}
+              </div>
+              <div className="todo__item__actions">
+                {edit !== todo.key && (
+                  <>
+                    <button
+                      onClick={() =>
+                        setTodos(todos.filter((e) => e.key !== todo.key))
+                      }
+                    >
+                      <AiOutlineDelete size={30} className="action-1" />
+                      <AiFillDelete size={30} className="action-2" />
+                    </button>
+                    <button
+                      onClick={() => {
+                        setEdit(todo.key);
+                        setEditValue(todo.value);
+                      }}
+                    >
+                      <AiOutlineEdit size={30} className="action-1" />
+                      <AiFillEdit size={30} className="action-2" />
+                    </button>
+                  </>
+                )}
+                {edit === todo.key && (
+                  <>
+                    <button onClick={updateTodo}>
+                      <AiOutlineSave size={30} className="action-1" />
+                      <AiFillSave size={30} className="action-2" />
+                    </button>
+                    <button
+                      onClick={() => {
+                        setEdit(todo.key);
+                        setEditValue(todo.value);
+                      }}
+                    >
+                      <AiOutlineCloseSquare size={30} className="action-1" />
+                      <AiFillCloseSquare size={30} className="action-2" />
+                    </button>
+                  </>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
-
-export default TodoList;
+};
